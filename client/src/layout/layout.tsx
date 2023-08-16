c1ffe5b7010878d5574503212e66574ae435294f
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2023-08-16 19:17:12 +0500

    layout componenet

--- client/src/layout/layout.tsx
+++ client/src/layout/layout.tsx
@@ -1,21 +1,17 @@
-import { FunctionComponent } from 'react'
-import { Footer, Header, Sidebar } from '.'
-import { LayoutProps } from './layout.props'
+import { FunctionComponent } from 'react';
+import { Footer, Header, Sidebar } from '.';
+import { LayoutProps } from './layout.props';
+import styles from './layout.module.css';
 
 
 const Layout = ({children}: LayoutProps):JSX.Element => {
   return (
-    <>
-        {/* Headers */}
-        <Header/>
-        <div>
-            {/*Sidebar  */}
-            <Sidebar/>
-            <div>{children}</div>
-        </div>
-        {/* Footer */}
-        <Footer/>
-    </>
+    <div className={styles.wrapper}>
+        <Header className={styles.header}/>
+        <Sidebar className={styles.sidebar}/>
+        <div className={styles.body}>{children}</div>
+        <Footer className={styles.footer}/>
+    </div>
   )
 }
 
