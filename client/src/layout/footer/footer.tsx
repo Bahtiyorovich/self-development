db7a90489498f0aa2c89146f24b20261d6b80179
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2023-08-17 08:33:13 +0500

    footer component

--- client/src/layout/footer/footer.tsx
+++ client/src/layout/footer/footer.tsx
@@ -1,8 +1,19 @@
-import { FooterProps } from './footer.props'
+import { FooterProps } from './footer.props';
+import styles from './footer.module.css';
+import cn from 'classnames';
+import {format} from 'date-fns';
 
-const Footer = ({...props}: FooterProps): JSX.Element => {
+const Footer = ({className,...props}: FooterProps): JSX.Element => {
   return (
-    <div {...props}>Footer</div>
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>
+        Next Level © 2022 - {format(new Date(), 'yyyy')}. All right reserved
+      </div>
+      <div>
+        <a href="#">Terms of use</a>
+        <a href='#'>Privacy Policy</a>
+      </div>
+    </footer>
   )
 }
 
