572ee7ac0892b7308f6f22ecad470fde769c7756
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2023-08-18 11:01:51 +0500

    ssr interface

--- client/src/pages/cource/[slug].tsx
+++ client/src/pages/cource/[slug].tsx
@@ -1,9 +1,11 @@
 import { MenuItem } from "@/interfaces/menu.interface";
+import { PageModel } from "@/interfaces/page.interface";
+import { ProductModel } from "@/interfaces/product.interface";
 import { withLayout } from "@/layout/layout"
 import axios from "axios";
 import { GetServerSideProps } from "next";
 
-const Index = ({menu, page, products}) => {
+const Index = ({menu, page, products}: PageProps): JSX.Element => {
   return (
     <div>Index</div>
   )
@@ -11,7 +13,7 @@ const Index = ({menu, page, products}) => {
 
 export default withLayout(Index); 
 
-export const getServerSideProps: GetServerSideProps = async ({query}) => {
+export const getServerSideProps: GetServerSideProps<PageProps> = async ({query}) => {
     const {slug} = query
     const firstCategory = 0
 
@@ -20,8 +22,8 @@ export const getServerSideProps: GetServerSideProps = async ({query}) => {
     }
 
     const {data: menu} = await axios.post<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find`, {firstCategory})
-    const {data: page} = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find/${slug}`)
-    const {data: products} = await axios.post(`${process.env.NEXT_PUBLIC_DOMAIN}/api/product-find`, {category: slug})
+    const {data: page} = await axios.get<PageModel[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find/${slug}`)
+    const {data: products} = await axios.post<ProductModel[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/product-find`, {category: slug})
 
 
     return {
@@ -31,4 +33,10 @@ export const getServerSideProps: GetServerSideProps = async ({query}) => {
             products,
         },
     }
+}
+
+interface PageProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  page: PageModel[];
+  products: ProductModel[];
 }
